fea042c56a62b3680f632357a1d74d50a7340e54
Yyax13 <Yyax13@proton.me>
2026-01-06 23:12:00 -0300

    FEAT - Created a simple app to print current OS

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,18 @@ void mainSetup() {
     // Blinks for 5 secs
     blinkAlert(5);
     
-    // _OS_ENUM os = detectOS();
+    _OS_ENUM os = detectOS();
     
-    // if (os == Linux) {
-    //     DigiKeyboardPtBr.println("Linux!");
+    if (os == Linux) {
+        DigiKeyboardPtBr.println("Linux!");
         
-    // } else if (os == Windows) {
-    //     DigiKeyboardPtBr.println("Windows!");
+    } else if (os == Windows) {
+        DigiKeyboardPtBr.println("Windows!");
         
-    // } else if (os == MacOS) {
-    //     DigiKeyboardPtBr.println("MacOS!");
+    } else if (os == MacOS) {
+        DigiKeyboardPtBr.println("MacOS!");
     
-    // }
-    
-    DigiKeyboardPtBr.print("ptbr abcçABC123!@/");
-    DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-    DigiKeyboardPtBr.sendKeyStroke(0x1e, 0x02 | 0xE000);
-    DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+    }
     
 }
 
