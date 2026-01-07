6e8f552c1cc79eaf0053dd8116d2c7fac204331a
Yyax13 <Yyax13@proton.me>
2026-01-07 15:59:13 -0300

    REF - Reworked class functions declaration to avoid bugs and used auth props

--- src/infra/TTY/main.cpp
+++ src/infra/TTY/main.cpp
@@ -1,40 +1,35 @@
 /*
- * WARNING: UNSTABLE, USE BY YOUR OWN RISK
- * 
  * TODO:
- *      - Change the class def and implementation, incorrect implementation here, .cpp should not declare a class
- *      - Add authentication variables to TTY class
+ *      
  */
 
 #include "./../../lib/DigiKeyboardPtBr.h"
+#include "main.h";
 
-class TTY {
-    public:
-        /// FuncToRunInTTY must write anything into tty
-        void runInTTY(void (*FuncToRunInTTY)(TTY&)) {
-            TTY::OpenTTY();
-            FuncToRunInTTY(*this);
-            TTY::CloseTTY();
+/// FuncToRunInTTY must write anything into tty
+void TTY::runInTTY(void (*FuncToRunInTTY)(TTY&)) {
+    TTY::OpenTTY();
+    FuncToRunInTTY(*this);
+    TTY::CloseTTY();
 
-        }
+}
 
-    private:
-        static void OpenTTY() {
-            DigiKeyboardPtBr.sendKeyStroke(KEY_F3, PTBR_MOD_CONTROL_LEFT | PTBR_MOD_ALT_LEFT);
-            DigiKeyboardPtBr.delay(2500);
-            DigiKeyboardPtBr.print("user");
-            DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-            DigiKeyboardPtBr.delay(1000);
-            DigiKeyboardPtBr.print("pass");
-            DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
-            DigiKeyboardPtBr.delay(750);
+void TTY::OpenTTY() {
+    DigiKeyboardPtBr.sendKeyStroke(KEY_F3, PTBR_MOD_CONTROL_LEFT | PTBR_MOD_ALT_LEFT);
+    DigiKeyboardPtBr.delay(2500);
+    DigiKeyboardPtBr.print(TTY::user);
+    DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+    DigiKeyboardPtBr.delay(1000);
+    DigiKeyboardPtBr.print(TTY::pass);
+    DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+    DigiKeyboardPtBr.delay(750);
 
-        }
+}
 
-        static void CloseTTY() {
-            DigiKeyboardPtBr.sendKeyStroke(KEY_F1, PTBR_MOD_CONTROL_LEFT | PTBR_MOD_ALT_LEFT);
-            DigiKeyboardPtBr.delay(3500); 
-        
-        }
+void TTY::CloseTTY() {
+    DigiKeyboardPtBr.sendKeyStroke(KEY_F1, PTBR_MOD_CONTROL_LEFT | PTBR_MOD_ALT_LEFT);
+    DigiKeyboardPtBr.delay(3500); 
 
-};
+}
+
+TTY tty = TTY();
